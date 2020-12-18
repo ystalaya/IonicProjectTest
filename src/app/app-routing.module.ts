88a42f415fcfd4473108eb546b5c75ca97a44562
ystalaya <ystalaya2009@gmail.com>
2020-12-18 09:41:29 +0300

    add pages, index, home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,48 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+let routes: Routes;
+routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   }
+  // {
+  //   path: 'index',
+  //   loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
+  // },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  // },
+  // {
+  //   path: 'signup',
+  //   loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  // },
+  // {
+  //   path: 'welcome',
+  //   loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)
+  // },
+  // {
+  //   path: 'feed',
+  //   loadChildren: () => import('./pages/feed/feed.module').then( m => m.FeedPageModule)
+  // },
+  // {
+  //   path: 'notifications',
+  //   loadChildren: () => import('./pages/notifications/notifications.module').then( m => m.NotificationsPageModule)
+  // },
+  // {
+  //   path: 'messages',
+  //   loadChildren: () => import('./pages/messages/messages.module').then( m => m.MessagesPageModule)
+  // },
+  // {
+  //   path: 'settings',
+  //   loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
+  // }
 ];
 @NgModule({
   imports: [
